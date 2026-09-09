78a7836cfba1b74604af87ffae9e1fc8f3a4a5fb
ricosar <ricos@sheridancollege.ca>
2026-09-09 17:21:32 -0400

    Added some more code to merge

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -5,13 +5,14 @@ public class Test : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Debug.Log("yay"); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("Hello world meowwwwwww!");
+        Debug.Log("Hello world meowwwwwww!!");
         Debug.Log("Testing to make sure I know how to use github!")
+        Debug.Log("Adding one more line")
     }
 }
